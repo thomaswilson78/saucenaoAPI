7826aaae9b51468c8ae16696dccbb6f68137ee8c
Thomas Wilson <15836278+thomaswilson78@users.noreply.github.com>
2024-03-16 00:54:52 -0400

    Fix to not removing images.

--- src/checkresults.py
+++ src/checkresults.py
@@ -26,13 +26,19 @@ results_query = """
 """
 
 
-def remove_file(image_uid:int):
-    imgDB.execute_change("DELETE FROM Images WHERE image_uid = ?", [image_uid])
+def remove_file(image:Image):
+    imgDB.execute_change("DELETE FROM Images WHERE image_uid = ?", [int(image.image_uid)])
+    if os.path.exists(image.full_path):
+        os.remove(image.full_path)
 
 
 def update_status(results:list[int]):
     imgDB.execute_change(f"UPDATE Saucenao_Results SET status = 1 WHERE result_uid IN ({','.join('?' for _ in results)})", results)
 
+
+def add_favorite(img_id):
+    danAPI.add_favorite(img_id)
+    print(f"{img_id} added to favorites.")
 
 def display_results(image:Image, results:list[Saucenao_Result]):
     webbrowser.get(config.settings["DEFAULT_BROWSER"]).open(image.full_path, new = 0)
@@ -44,21 +50,21 @@ def display_results(image:Image, results:list[Saucenao_Result]):
 
     try:
         while True:
-            input_val = input(f"Enter result(s) that match separated by ','. (n - none/q - quit): ").lower()
+            input_val = input(f"Enter result(s) that match separated by ','. (a - all/n - none): ").lower()
             match input_val:
                 case "n":
                     update_status([r.result_uid for r in results])
                     break
-                case "q":
-                    print("Exited.")
-                    exit()
+                case "a":
+                    for r in results:
+                        add_favorite(r.site_id)
+                    remove_file(image)
+                    break
                 case _:
                     try:
                         for i in [int(i.strip()) for i in input_val.split(",")]:
-                            dan_id = results[i].site_id
-                            danAPI.add_favorite(dan_id)
-                            print(f"{dan_id} added to favorites.")
-                        remove_file(image.image_uid)
+                            add_favorite(results[i].site_id)
+                        remove_file(image)
                         break
                     except:
                         print("Invalid input.")
@@ -71,7 +77,7 @@ def check_low_threshold_results(threshold:float):
         image_list:list[Image] = [Image(r) for r in imgDB.execute_query("""SELECT * FROM Images""")]
         for i in image_list:
             if not os.path.exists(i.full_path):
-                remove_file(i.image_uid)
+                remove_file(i)
                 print(f"{i.file_name} already deleted. Removed entry.")
                 continue
 
